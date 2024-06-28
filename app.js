8d41334679cb940f09e03d9ccd1811ea966149eb
beprodeep4718 <144547279+beprodeep4718@users.noreply.github.com>
2024-06-28 20:57:11 +0530

    cors

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const connectDB = require('./utils/db');
-const authRouter = require('./routes/auth-route');
+const cors = require("cors");
+const connectDB = require("./utils/db");
+const authRouter = require("./routes/auth-route");
 
-const PORT = 3000 || process.env.PORT ;
+const PORT = 3000 || process.env.PORT;
 
 const corsOptions = {
-    origin: ['https://www.anjusa.in/', 'https://www.anjusaacademy.com/'],
-    methods: "GET, POST, PUT, DELETE, PATCH",
-    credentials: true,
-}
+  origin: "https://www.anjusa.in", // Allow requests from this origin
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
 app.use(cors(corsOptions));
 
 app.use(express.json());
 
-app.use('/api/auth', authRouter);
+app.use("/api/auth", authRouter);
 
 const start = () => {
-    try {
-        app.listen(PORT, () => {
-            console.log(`listening on ${PORT}`);
-        })
-        connectDB(process.env.MONGO_URI);
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    app.listen(PORT, () => {
+      console.log(`listening on ${PORT}`);
+    });
+    connectDB(process.env.MONGO_URI);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 start();
