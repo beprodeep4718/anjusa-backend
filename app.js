c1678230c2c452241f326dc820983e5ae27f1659
beprodeep4718 <144547279+beprodeep4718@users.noreply.github.com>
2024-06-29 22:34:28 +0530

    done

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const PORT = 3000 || process.env.PORT;
 
 // List of allowed origins
 const allowedOrigins = [
-  'https://www.anjusa.in',
-  'https://www.anjusaacademy.com/',
-  'http://localhost:5173/'
+  "https://www.anjusa.in",
+  "https://www.anjusaacademy.com/",
+  "http://localhost:5173/",
 ];
 
 const corsOptions = {
@@ -22,7 +22,7 @@ const corsOptions = {
     if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
   },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
